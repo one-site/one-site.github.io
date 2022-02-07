52e0b8fb727a98cc1b65e07e06e7da21a28178cc
randallr <rdaf.richard@gmail.com>
2022-02-07 15:17:51 -0500

    Add src files

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import m from "mithril";
+import get from 'lodash-es/get'
+import groupBy from 'lodash-es/groupBy'
+import mapValues from 'lodash-es/mapValues'
+
+import Grouped from "./Grouped";
+
+import {format, parseISO, startOfMonth, differenceInCalendarDays} from 'date-fns'
+
+function nest(seq, keys) {
+  if (!keys.length) {
+    return seq;
+  }
+  var first = keys[0];
+
+  //.count = first.count === undefined ? 1 : first.count++
+  var rest = keys.slice(1);
+  return mapValues(groupBy(seq, first), function (value) {
+    return nest(value, rest)
+  });
+};
+
+let allEvents = []
+let requestOptions = {background: true}
+let dataBaseURL = 'https://one-site.github.io'
+let nestedData = {}
+
+Promise.all([
+    m.request(`${dataBaseURL}/data/ertm.json`, requestOptions),
+    m.request(`${dataBaseURL}/data/mrs.json`, requestOptions),
+    m.request(`${dataBaseURL}/data/mvshofs.json`, requestOptions),
+    m.request(`${dataBaseURL}/data/nwmt.json`, requestOptions),
+    m.request(`${dataBaseURL}/data/nyirp.json`, requestOptions),
+    m.request(`${dataBaseURL}/data/opnm.json`, requestOptions)
+      .then(it => it.map(race => Object.assign(race, {series: 'OPNM'}))),
+    m.request(`${dataBaseURL}/data/roc.json`, requestOptions)
+      .then(it => it.map(race => Object.assign(race, {series: 'ROC'}))),
+    m.request(`${dataBaseURL}/data/smart.json`, requestOptions),
+    m.request(`${dataBaseURL}/data/ttom.json`, requestOptions)
+      .then(it => it.map(race => Object.assign(race, {series: 'TTOM'}))),
+  ]
+)
+  .then(it => {
+      allEvents = it.flatMap(series => series)
+      allEvents.reduce((agg, row, i) => {
+        row.date = parseISO(row.date)
+        row.id = i
+        return agg
+      }, allEvents)
+
+      allEvents = allEvents.sort((r1, r2) => r1.date - r2.date)
+
+      nestedData = nest(allEvents.map(it => {
+        it.m = format(startOfMonth(it.date), 'yyyy-MM-dd')
+        it.dt = format(it.date, 'dd-MMM-yyyy')
+        return it
+      }), ['m', 'dt'])
+
+      m.redraw()
+    }
+  )
+
+m.mount(document.body, {
+    view: () => {
+      return m(Grouped, {nestedData: nestedData})
+    }
+  }
+)
+
+// window.DEBUG = {
+//   m: m
+// }
---
+++ src/Grouped.js
@@ -0,0 +1,109 @@
+import m from 'mithril';
+import b from 'bss';
+import {format, parseISO} from "date-fns";
+
+function mtfunc(obj, count = 0) {
+  let levelCount = count
+  if (Array.isArray(obj)) return obj.length
+
+  Object.values(obj).forEach(it => {
+      let ret = mtfunc(it)
+      levelCount += ret
+    }
+  )
+  return levelCount
+}
+
+const Grouped = () => {
+
+  let body = b({
+    height: '100%',
+    width: '100%',
+    // 'overflow-y': 'auto',
+  }).class
+
+  let grid = b({
+    //height: '100%',
+    //width: '100%',
+    //'overflow-y': 'auto',
+    'display': 'grid',
+    'grid-template-columns': '1fr 1fr 1fr 1fr',
+    //'grid-auto-rows': '50px'
+    'overflow-y': 'auto',
+    height: '100%'
+  }).class
+
+  let span_mnth = b({
+    'margin-left': '0.25em'
+  })
+
+  let span_dt = b({
+    'margin-left': '0.5em'
+  })
+
+  let _4cols = b({
+    'grid-column': '1 / -1',
+    'background-color': 'lightgrey',
+    //position: 'sticky',
+    //top:0,
+    //left:0
+  })
+
+  document.body.classList.add(body)
+
+  let rowsCount = 1
+
+  return {
+
+    view: ({attrs: {nestedData}}) => {
+      // console.log('Grouped::view', nestedData)
+      return m(`div.${grid}`, Object.keys(nestedData)
+        .map((it, row) => {
+
+          let _area1 = b({
+            'background-color': 'lightgrey',
+            'grid-area': `${rowsCount}/1/${rowsCount + 1}/-1`,
+            position: 'sticky',
+            top: 0,
+            left: 0
+          })
+
+          rowsCount++
+
+          let days = Object.keys(nestedData[it]).flatMap(day => {
+            let values = nestedData[it][day]
+            // console.log('day', day, values)
+            // console.log(`${rowsCount}/1/${rowsCount + 1}/-1`)
+
+            return values.map((_day, i) => {
+              // console.log('mtfunc', mtfunc(values))
+              let _area2 = b({
+                'font-size': '0.85em',
+                'grid-row': `${rowsCount}/${rowsCount + 1}`
+              })
+
+              rowsCount++
+
+              return [
+                !(i > 0) && m(`div.${_area2}`, {style: 'grid-column:1/2'}, m(`span.${span_dt}`, format(_day.date, 'EEE, do'))),
+                m(`div.${_area2}`, {style: 'grid-column:2/3'}, _day.series),
+                m(`div.${_area2}`, {style: 'grid-column:3/5'}, _day.track),
+                //m(`div.${_area2}`, {style:'grid-column:4/5'}, 'x')
+              ]
+
+            })
+          })
+
+          return [
+            m(`div.${_area1}`, m(`span.${span_mnth}`, format(parseISO(it), 'MMMM YYY')))
+          ].concat(days)
+
+        })
+      )
+    }
+
+
+  }
+}
+
+export default Grouped;
