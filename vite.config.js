b4d6976ca5b9753ae95b136b41e85924d97bbdcb
randallr <rdaf.richard@gmail.com>
2026-03-18 20:36:50 -0400

    /data move

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
+import {resolve} from "path";
 
 export default defineConfig({
   // Settings
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        'fuel-calculator': resolve(__dirname, "fuel-calculator.html"),
+      }
+    }
+  }
 });
